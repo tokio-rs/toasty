a95aa51e40a4b9881bad4d78f810048f6d27fd67
Open Schnick <60597856+open-schnick@users.noreply.github.com>
2024-10-25 15:42:46 +0000

    refactor(sqlite): rename Sqlite::new() to Sqlite::in_memory() (#25)
    one has to read the new function to understand that the default behavior
    is in-memory

--- tests/client/src/db.rs
+++ tests/client/src/db.rs
@@ -46,7 +46,7 @@ pub struct SetupSqlite;
 #[async_trait::async_trait]
 impl Setup for SetupSqlite {
     async fn setup(&self, schema: Schema) -> Db {
-        let driver = toasty_sqlite::Sqlite::new();
+        let driver = toasty_sqlite::Sqlite::in_memory();
         let db = toasty::Db::new(schema, driver).await;
         db.reset_db().await.unwrap();
         db
