f67c05e781f57b73011e79dd323ae551fb241857
Josh McKinney <joshka@users.noreply.github.com>
2024-11-04 15:21:31 -0800

    Add color to cli help output (#46)
    This is just using the cargo styling for now.

--- src/cli/src/main.rs
+++ src/cli/src/main.rs
@@ -1,10 +1,30 @@
 use toasty_cli::{gen, init};
 
 use anyhow::Result;
-use clap::{Parser, Subcommand};
+use clap::{
+    builder::styling::{AnsiColor, Style, Styles},
+    Parser, Subcommand,
+};
+
+pub const HEADER: Style = AnsiColor::Green.on_default().bold();
+pub const USAGE: Style = AnsiColor::Green.on_default().bold();
+pub const LITERAL: Style = AnsiColor::Cyan.on_default().bold();
+pub const PLACEHOLDER: Style = AnsiColor::Cyan.on_default();
+pub const ERROR: Style = AnsiColor::Red.on_default().bold();
+pub const VALID: Style = AnsiColor::Cyan.on_default().bold();
+pub const INVALID: Style = AnsiColor::Yellow.on_default().bold();
+
+pub const HELP_STYLES: Styles = Styles::styled()
+    .header(HEADER)
+    .usage(USAGE)
+    .literal(LITERAL)
+    .placeholder(PLACEHOLDER)
+    .error(ERROR)
+    .valid(VALID)
+    .invalid(INVALID);
 
 #[derive(Parser, Debug)]
-#[clap(version, about)]
+#[clap(version, about, styles = HELP_STYLES)]
 struct Cli {
     #[clap(subcommand)]
     command: Command,
