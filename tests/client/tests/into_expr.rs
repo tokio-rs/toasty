f195cc5d96fa3a138029f9d3c8889682882d4a9d
Carl Lerche <me@carllerche.com>
2025-03-11 10:16:48 -0700

    wip

--- tests/client/tests/into_expr.rs
+++ tests/client/tests/into_expr.rs
@@ -1,32 +1,32 @@
 use tests_client::*;
+use toasty::stmt::Id;
 
 async fn string_to_id_expr(s: impl Setup) {
-    schema!(
-        "
-        model Foo {
-            #[key]
-            #[auto]
-            id: Id,
-        }"
-    );
+    #[derive(Debug)]
+    #[toasty::model]
+    struct Foo {
+        #[key]
+        #[auto]
+        id: Id<Self>,
+    }
 
-    let db = s.setup(db::load_schema()).await;
+    let db = s.setup(models!(Foo)).await;
 
     #[allow(clippy::disallowed_names)]
-    let foo = db::Foo::create().exec(&db).await.unwrap();
+    let foo = Foo::create().exec(&db).await.unwrap();
 
     let id = foo.id.to_string();
 
     // Find the record using the ID as a &String
-    let foo2 = db::Foo::get_by_id(&db, &id).await.unwrap();
+    let foo2 = Foo::get_by_id(&db, &id).await.unwrap();
     assert_eq!(foo2.id, foo.id);
 
     // Find the record using the ID as a &str
-    let foo2 = db::Foo::get_by_id(&db, &id[..]).await.unwrap();
+    let foo2 = Foo::get_by_id(&db, &id[..]).await.unwrap();
     assert_eq!(foo2.id, foo.id);
 
     // Find the record using the ID as a String
-    let foo2 = db::Foo::get_by_id(&db, id).await.unwrap();
+    let foo2 = Foo::get_by_id(&db, id).await.unwrap();
     assert_eq!(foo2.id, foo.id);
 }
 
