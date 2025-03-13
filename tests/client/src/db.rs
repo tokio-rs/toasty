f1a3288d68b6a1d30d4a6aae9d96281efebead13
Hayden Young <22327045+hbjydev@users.noreply.github.com>
2025-03-13 23:59:36 +0000

    feat: include libsql in test client lib

--- tests/client/src/db.rs
+++ tests/client/src/db.rs
@@ -3,3 +3,6 @@ pub mod dynamodb;
 
 #[cfg(feature = "sqlite")]
 pub mod sqlite;
+
+#[cfg(feature = "libsql")]
+pub mod libsql;
---
+++ tests/client/src/db/libsql.rs
@@ -0,0 +1,21 @@
+use toasty::driver::Capability;
+use toasty::schema::app::Schema;
+use toasty::Db;
+
+use crate::Setup;
+
+pub struct SetupLibSQL;
+
+#[async_trait::async_trait]
+impl Setup for SetupLibSQL {
+    async fn setup(&self, schema: Schema) -> Db {
+        let driver = toasty_libsql::LibSQL::local(":memory:".to_string()).await.unwrap();
+        let db = toasty::Db::new(schema, driver).await.unwrap();
+        db.reset_db().await.unwrap();
+        db
+    }
+
+    fn capability(&self) -> &Capability {
+        &Capability::Sql
+    }
+}
