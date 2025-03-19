f2f0f50b282f5fb7fb7cc368a86899c1102c4831
Carl Lerche <me@carllerche.com>
2025-03-19 10:29:51 -0700

    more err info

--- src/toasty/src/driver.rs
+++ src/toasty/src/driver.rs
@@ -32,7 +32,7 @@ impl Connection {
             "dynamodb" => Self::connect_dynamodb(&url).await,
             "postgresql" => Self::connect_postgresql(&url).await,
             "sqlite" => Self::connect_sqlite(&url),
-            _ => Err(anyhow::anyhow!("unsupported database; url={url}")),
+            scheme => Err(anyhow::anyhow!("unsupported database; schema={scheme}; url={url}")),
         }
     }
 
