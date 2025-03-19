6ceebffdddcafc7f6f7f7ecfb98b25ed7234f3c9
Hayden Young <22327045+hbjydev@users.noreply.github.com>
2025-03-19 17:19:15 +0000

    fix: use new test harness setup signature

--- tests/client/src/db/libsql.rs
+++ tests/client/src/db/libsql.rs
@@ -1,6 +1,5 @@
 use toasty::driver::Capability;
-use toasty::schema::app::Schema;
-use toasty::Db;
+use toasty::{db, Db};
 
 use crate::Setup;
 
@@ -8,11 +7,8 @@ pub struct SetupLibSQL;
 
 #[async_trait::async_trait]
 impl Setup for SetupLibSQL {
-    async fn setup(&self, schema: Schema) -> Db {
-        let driver = toasty_libsql::LibSQL::local(":memory:".to_string())
-            .await
-            .unwrap();
-        let db = toasty::Db::new(schema, driver).await.unwrap();
+    async fn setup(&self, mut builder: db::Builder) -> Db {
+        let db = builder.connect("libsql::memory:").await.unwrap();
         db.reset_db().await.unwrap();
         db
     }
