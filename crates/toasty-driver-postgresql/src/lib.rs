108936b4ed188d9f8f6a37fafb9725eb440ec0cc
Carl Lerche <me@carllerche.com>
2025-07-31 10:39:57 -0700

    fix tests

--- crates/toasty-driver-postgresql/src/lib.rs
+++ crates/toasty-driver-postgresql/src/lib.rs
@@ -168,9 +168,9 @@ impl Driver for PostgreSQL {
     }
 
     async fn exec(&self, schema: &Arc<Schema>, op: Operation) -> Result<Response> {
-        let sql: sql::Statement = match op {
-            Operation::Insert(op) => op.stmt.into(),
-            Operation::QuerySql(query) => query.stmt.into(),
+        let (sql, ret_tys): (sql::Statement, _) = match op {
+            Operation::Insert(op) => (op.stmt.into(), None),
+            Operation::QuerySql(query) => (query.stmt.into(), query.ret),
             op => todo!("op={:#?}", op),
         };
 
@@ -205,11 +205,12 @@ impl Driver for PostgreSQL {
                 anyhow::bail!("update condition did not match");
             }
         } else {
+            let ret_tys = ret_tys.as_ref().unwrap().clone();
             let results = rows.into_iter().map(move |row| {
                 let mut results = Vec::new();
                 for i in 0..row.len() {
                     let column = &row.columns()[i];
-                    results.push(postgres_to_toasty(i, &row, column));
+                    results.push(postgres_to_toasty(i, &row, column, &ret_tys[i]));
                 }
 
                 Ok(ValueRecord::from_vec(results))
@@ -232,7 +233,12 @@ impl Driver for PostgreSQL {
 }
 
 /// Converts a PostgreSQL value within a row to a [`toasty_core::stmt::Value`].
-fn postgres_to_toasty(index: usize, row: &Row, column: &Column) -> stmt::Value {
+fn postgres_to_toasty(
+    index: usize,
+    row: &Row,
+    column: &Column,
+    expected_ty: &stmt::Type,
+) -> stmt::Value {
     // NOTE: unfortunately, the inner representation of the PostgreSQL type enum is not
     // accessible, so we must manually match each type like so.
     if column.type_() == &Type::TEXT || column.type_() == &Type::VARCHAR {
@@ -245,7 +251,11 @@ fn postgres_to_toasty(index: usize, row: &Row, column: &Column) -> stmt::Value {
             .unwrap_or(stmt::Value::Null)
     } else if column.type_() == &Type::INT2 {
         row.get::<usize, Option<i16>>(index)
-            .map(|v| stmt::Value::I8(v as i8))
+            .map(|v| match expected_ty {
+                stmt::Type::I8 => stmt::Value::I8(v as i8),
+                stmt::Type::I16 => stmt::Value::I16(v),
+                _ => todo!("unexpected type for INT2: {expected_ty:#?}"),
+            })
             .unwrap_or(stmt::Value::Null)
     } else if column.type_() == &Type::INT4 {
         row.get::<usize, Option<i32>>(index)
