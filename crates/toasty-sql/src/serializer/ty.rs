233803ed8c6cdce0cc5f7c0beea2ce799bbbe7ab
Carl Lerche <me@carllerche.com>
2025-08-02 11:46:04 -0700

    wip

--- crates/toasty-sql/src/serializer/ty.rs
+++ crates/toasty-sql/src/serializer/ty.rs
@@ -12,20 +12,18 @@ impl ToSql for &db::Type {
             db::Type::Integer(_) => todo!(),
             db::Type::UnsignedInteger(size) => {
                 match f.serializer.flavor {
-                    Flavor::Mysql => {
-                        match size {
-                            1 => fmt!(f, "TINYINT UNSIGNED"),
-                            2 => fmt!(f, "SMALLINT UNSIGNED"),
-                            3..=4 => fmt!(f, "INT UNSIGNED"),
-                            5..=8 => fmt!(f, "BIGINT UNSIGNED"),
-                            _ => todo!("Unsupported unsigned integer size: {}", size),
-                        }
-                    }
+                    Flavor::Mysql => match size {
+                        1 => fmt!(f, "TINYINT UNSIGNED"),
+                        2 => fmt!(f, "SMALLINT UNSIGNED"),
+                        3..=4 => fmt!(f, "INT UNSIGNED"),
+                        5..=8 => fmt!(f, "BIGINT UNSIGNED"),
+                        _ => todo!("Unsupported unsigned integer size: {}", size),
+                    },
                     Flavor::Postgresql => {
                         match size {
-                            1 => fmt!(f, "SMALLINT"), // u8 -> SMALLINT (i16)
-                            2 => fmt!(f, "INTEGER"),  // u16 -> INTEGER (i32)
-                            3..=4 => fmt!(f, "BIGINT"), // u32 -> BIGINT (i64)
+                            1 => fmt!(f, "SMALLINT"),    // u8 -> SMALLINT (i16)
+                            2 => fmt!(f, "INTEGER"),     // u16 -> INTEGER (i32)
+                            3..=4 => fmt!(f, "BIGINT"),  // u32 -> BIGINT (i64)
                             5..=8 => fmt!(f, "NUMERIC"), // u64 -> NUMERIC (arbitrary precision)
                             _ => todo!("Unsupported unsigned integer size: {}", size),
                         }
