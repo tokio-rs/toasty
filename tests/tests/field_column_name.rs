93049baa8bcfc7c8a026644b0a883637a36c81f0
Julien Scholz <8947616+pikaju@users.noreply.github.com>
2025-11-13 17:50:22 +0100

    Add test for #[column] with name and type

--- tests/tests/field_column_name.rs
+++ tests/tests/field_column_name.rs
@@ -14,10 +14,8 @@ async fn specify_custom_column_name(test: &mut DbTest) {
 
     let db = test.setup_db(models!(User)).await;
 
-    eprintln!("kek1");
     let u = User::create().name("foo").exec(&db).await.unwrap();
     assert_eq!(u.name, "foo");
-    eprintln!("kek2");
 
     // Make sure the column has actually been renamed to my_name in the underlying database.
     assert_eq!(
@@ -28,4 +26,36 @@ async fn specify_custom_column_name(test: &mut DbTest) {
     );
 }
 
-tests!(specify_custom_column_name,);
+async fn specify_custom_column_name_with_type(test: &mut DbTest) {
+    #[derive(toasty::Model)]
+    struct User {
+        #[key]
+        #[auto]
+        id: Id<Self>,
+
+        #[column("my_name", type = varchar(5))]
+        name: String,
+    }
+
+    let db = test.setup_db(models!(User)).await;
+
+    let u = User::create().name("foo").exec(&db).await.unwrap();
+    assert_eq!(u.name, "foo");
+
+    // Make sure the column has actually been renamed to my_name in the underlying database.
+    assert_eq!(
+        test.get_raw_column_value::<String>("users", "my_name", Default::default())
+            .await
+            .unwrap(),
+        "foo"
+    );
+
+    // Creating a user with a name larger than 5 characters should fail.
+    let res = User::create().name("foo bar").exec(&db).await;
+    assert!(res.is_err());
+}
+
+tests!(
+    specify_custom_column_name,
+    specify_custom_column_name_with_type
+);
