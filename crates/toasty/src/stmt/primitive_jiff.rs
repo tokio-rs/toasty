bb6ceab9cac389bc0878dc1090e617e93d7fcf2c
Julien Scholz <8947616+pikaju@users.noreply.github.com>
2025-11-27 00:40:58 +0100

    Implement jiff primitives

---
+++ crates/toasty/src/stmt/primitive_jiff.rs
@@ -0,0 +1,28 @@
+use crate::stmt::Primitive;
+use toasty_core::{
+    stmt::{Type, Value},
+    Result,
+};
+
+macro_rules! impl_jiff_primitive {
+    ($ty:ty, $name:ident, $lit:literal) => {
+        impl Primitive for $ty {
+            fn ty() -> Type {
+                Type::$name
+            }
+
+            fn load(value: Value) -> Result<Self> {
+                match value {
+                    Value::$name(v) => Ok(v),
+                    _ => anyhow::bail!("cannot convert value to {} {value:#?}", $lit),
+                }
+            }
+        }
+    };
+}
+
+impl_jiff_primitive!(jiff::Timestamp, Timestamp, "jiff::Timestamp");
+impl_jiff_primitive!(jiff::Zoned, Zoned, "jiff::Zoned");
+impl_jiff_primitive!(jiff::civil::Date, Date, "jiff::civil::Date");
+impl_jiff_primitive!(jiff::civil::Time, Time, "jiff::civil::Time");
+impl_jiff_primitive!(jiff::civil::DateTime, DateTime, "jiff::civil::DateTime");
