e2384c2694a8ed650b4124feae482efb1355ea32
Julien Scholz <8947616+pikaju@users.noreply.github.com>
2025-12-13 13:32:36 +0100

    Remove dangerous enum pattern

--- crates/toasty-driver-dynamodb/src/value.rs
+++ crates/toasty-driver-dynamodb/src/value.rs
@@ -24,25 +24,25 @@ impl Value {
     /// Converts a DynamoDB AttributeValue to a Toasty value.
     pub fn from_ddb(ty: &stmt::Type, val: &AttributeValue) -> Self {
         use stmt::Type;
-        use AttributeValue::*;
+        use AttributeValue as AV;
 
         let core_value = match (ty, val) {
-            (Type::Bool, Bool(val)) => stmt::Value::from(*val),
-            (Type::String, S(val)) => stmt::Value::from(val.clone()),
-            (Type::I8, N(val)) => stmt::Value::from(val.parse::<i8>().unwrap()),
-            (Type::I16, N(val)) => stmt::Value::from(val.parse::<i16>().unwrap()),
-            (Type::I32, N(val)) => stmt::Value::from(val.parse::<i32>().unwrap()),
-            (Type::I64, N(val)) => stmt::Value::from(val.parse::<i64>().unwrap()),
-            (Type::U8, N(val)) => stmt::Value::from(val.parse::<u8>().unwrap()),
-            (Type::U16, N(val)) => stmt::Value::from(val.parse::<u16>().unwrap()),
-            (Type::U32, N(val)) => stmt::Value::from(val.parse::<u32>().unwrap()),
-            (Type::U64, N(val)) => stmt::Value::from(val.parse::<u64>().unwrap()),
-            (Type::Bytes, B(val)) => stmt::Value::Bytes(val.clone().into_inner()),
-            (Type::Uuid, S(val)) => stmt::Value::from(val.parse::<uuid::Uuid>().unwrap()),
-            (Type::Id(model), S(val)) => {
+            (Type::Bool, AV::Bool(val)) => stmt::Value::from(*val),
+            (Type::String, AV::S(val)) => stmt::Value::from(val.clone()),
+            (Type::I8, AV::N(val)) => stmt::Value::from(val.parse::<i8>().unwrap()),
+            (Type::I16, AV::N(val)) => stmt::Value::from(val.parse::<i16>().unwrap()),
+            (Type::I32, AV::N(val)) => stmt::Value::from(val.parse::<i32>().unwrap()),
+            (Type::I64, AV::N(val)) => stmt::Value::from(val.parse::<i64>().unwrap()),
+            (Type::U8, AV::N(val)) => stmt::Value::from(val.parse::<u8>().unwrap()),
+            (Type::U16, AV::N(val)) => stmt::Value::from(val.parse::<u16>().unwrap()),
+            (Type::U32, AV::N(val)) => stmt::Value::from(val.parse::<u32>().unwrap()),
+            (Type::U64, AV::N(val)) => stmt::Value::from(val.parse::<u64>().unwrap()),
+            (Type::Bytes, AV::B(val)) => stmt::Value::Bytes(val.clone().into_inner()),
+            (Type::Uuid, AV::S(val)) => stmt::Value::from(val.parse::<uuid::Uuid>().unwrap()),
+            (Type::Id(model), AV::S(val)) => {
                 stmt::Value::from(stmt::Id::from_string(*model, val.clone()))
             }
-            (Type::Enum(..), S(val)) => {
+            (Type::Enum(..), AV::S(val)) => {
                 let (variant, rest) = val.split_once("#").unwrap();
                 let variant: usize = variant.parse().unwrap();
                 let v: V = serde_json::from_str(rest).unwrap();
@@ -85,20 +85,22 @@ impl Value {
 
     /// Converts this value to a DynamoDB AttributeValue.
     pub fn to_ddb(&self) -> AttributeValue {
+        use AttributeValue as AV;
+
         match &self.0 {
-            stmt::Value::Bool(val) => AttributeValue::Bool(*val),
-            stmt::Value::String(val) => AttributeValue::S(val.to_string()),
-            stmt::Value::I8(val) => AttributeValue::N(val.to_string()),
-            stmt::Value::I16(val) => AttributeValue::N(val.to_string()),
-            stmt::Value::I32(val) => AttributeValue::N(val.to_string()),
-            stmt::Value::I64(val) => AttributeValue::N(val.to_string()),
-            stmt::Value::U8(val) => AttributeValue::N(val.to_string()),
-            stmt::Value::U16(val) => AttributeValue::N(val.to_string()),
-            stmt::Value::U32(val) => AttributeValue::N(val.to_string()),
-            stmt::Value::U64(val) => AttributeValue::N(val.to_string()),
-            stmt::Value::Bytes(val) => AttributeValue::B(val.clone().into()),
-            stmt::Value::Uuid(val) => AttributeValue::S(val.to_string()),
-            stmt::Value::Id(val) => AttributeValue::S(val.to_string()),
+            stmt::Value::Bool(val) => AV::Bool(*val),
+            stmt::Value::String(val) => AV::S(val.to_string()),
+            stmt::Value::I8(val) => AV::N(val.to_string()),
+            stmt::Value::I16(val) => AV::N(val.to_string()),
+            stmt::Value::I32(val) => AV::N(val.to_string()),
+            stmt::Value::I64(val) => AV::N(val.to_string()),
+            stmt::Value::U8(val) => AV::N(val.to_string()),
+            stmt::Value::U16(val) => AV::N(val.to_string()),
+            stmt::Value::U32(val) => AV::N(val.to_string()),
+            stmt::Value::U64(val) => AV::N(val.to_string()),
+            stmt::Value::Bytes(val) => AV::B(val.clone().into()),
+            stmt::Value::Uuid(val) => AV::S(val.to_string()),
+            stmt::Value::Id(val) => AV::S(val.to_string()),
             stmt::Value::Enum(val) => {
                 let v = match &val.fields[..] {
                     [] => V::Null,
@@ -115,7 +117,7 @@ impl Value {
                     [stmt::Value::Id(id)] => V::Id(id.model_id().0, id.to_string()),
                     _ => todo!("val={:#?}", val.fields),
                 };
-                AttributeValue::S(format!(
+                AV::S(format!(
                     "{}#{}",
                     val.variant,
                     serde_json::to_string(&v).unwrap()
