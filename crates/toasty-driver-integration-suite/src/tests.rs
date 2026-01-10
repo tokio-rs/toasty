eaec131b198674ba12d5ce7fe80bc9e167be8049
Carl Lerche <me@carllerche.com>
2026-01-09 16:46:03 -0800

    Move more stuff

--- crates/toasty-driver-integration-suite/src/tests.rs
+++ crates/toasty-driver-integration-suite/src/tests.rs
@@ -8,6 +8,7 @@ pub mod field_column_name;
 pub mod field_column_type;
 pub mod has_many_batch_create;
 pub mod has_many_crud_basic;
+pub mod has_many_crud_multi_relations;
 pub mod has_many_n_1;
 pub mod has_one_crud_basic;
 pub mod one_model_crud;
