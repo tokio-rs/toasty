6cc749186a1932f53795d9250bd8596883525705
Carl Lerche <me@carllerche.com>
2026-02-03 09:45:33 -0800

    Move embedded codegen stub

--- crates/toasty-macros/src/lib.rs
+++ crates/toasty-macros/src/lib.rs
@@ -2,7 +2,6 @@ extern crate proc_macro;
 
 use proc_macro::TokenStream;
 use quote::quote;
-use syn;
 
 #[proc_macro_derive(
     Model,
@@ -17,46 +16,10 @@ pub fn derive_model(input: TokenStream) -> TokenStream {
 
 #[proc_macro_derive(Embed, attributes(column))]
 pub fn derive_embed(input: TokenStream) -> TokenStream {
-    let input = syn::parse_macro_input!(input as syn::DeriveInput);
-    let name = &input.ident;
-
-    let expanded = quote! {
-        const _: () = {
-            use toasty as _toasty;
-
-            impl _toasty::Model for #name {
-                type Query = ();
-                type Create = ();
-                type Update<'a> = ();
-                type UpdateQuery = ();
-
-                fn id() -> _toasty::codegen_support::ModelId {
-                    _toasty::codegen_support::generate_unique_id()
-                }
-
-                fn load(_row: _toasty::codegen_support::ValueRecord) -> Result<Self, _toasty::Error> {
-                    panic!("embedded types cannot be loaded directly")
-                }
-
-                fn schema() -> _toasty::codegen_support::schema::app::Model {
-                    use _toasty::codegen_support::schema;
-
-                    // For now, return a minimal embedded model with no fields
-                    // This is a no-op registration
-                    schema::app::Model {
-                        id: Self::id(),
-                        name: schema::Name::new(stringify!(#name)),
-                        fields: vec![],
-                        kind: schema::app::ModelKind::Embedded,
-                        indices: vec![],
-                        table_name: None,
-                    }
-                }
-            }
-        };
-    };
-
-    expanded.into()
+    match toasty_codegen::generate_embed(input.into()) {
+        Ok(output) => output.into(),
+        Err(e) => e.to_compile_error().into(),
+    }
 }
 
 #[proc_macro]
