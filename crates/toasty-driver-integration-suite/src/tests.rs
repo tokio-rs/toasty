4cd8adf14447b5b6120326fd4b9fa5b71c3324b6
Carl Lerche <me@carllerche.com>
2026-02-22 17:52:56 -0800

    rename tests

--- crates/toasty-driver-integration-suite/src/tests.rs
+++ crates/toasty-driver-integration-suite/src/tests.rs
@@ -3,7 +3,7 @@ pub mod belongs_to_one_way;
 pub mod belongs_to_self_referential;
 pub mod bigdecimal;
 pub mod decimal;
-pub mod embedded_enum;
+pub mod embedded_enum_unit;
 pub mod embedded_struct;
 pub mod field_auto;
 pub mod field_column_name;
