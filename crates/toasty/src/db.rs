52c3088894852f83d667e4c37394321e393e4a12
Joey de Waal <dewaaljoey@gmail.com>
2026-04-20 15:31:43 +0200

    chore: add test

--- crates/toasty/src/db.rs
+++ crates/toasty/src/db.rs
@@ -168,3 +168,22 @@ impl Executor for Db {
         Db::schema(self)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::Db;
+
+    #[test]
+    fn this_should_compile() {
+        #[allow(unused)]
+        async fn shared_ref(db: &Db) -> crate::Result<()> {
+            let tx = db.transaction().await?;
+            std::mem::drop(tx);
+
+            let tx = db.transaction_builder().begin().await?;
+            std::mem::drop(tx);
+
+            Ok(())
+        }
+    }
+}
