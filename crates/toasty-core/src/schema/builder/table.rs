dc34ef665e958a290e4f4598722d533e86ce3af3
Nickesh <nick9822@gmail.com>
2026-06-04 18:32:34 +0530

    feat: allow index on unit enum

--- crates/toasty-core/src/schema/builder/table.rs
+++ crates/toasty-core/src/schema/builder/table.rs
@@ -348,6 +348,12 @@ impl BuildTableFromModels<'_> {
         loop {
             match mapping {
                 mapping::Field::Primitive(p) => return p.column,
+                mapping::Field::Enum(p) => {
+                    if p.sub_projection.is_identity() {
+                        return p.discriminant.column;
+                    }
+                    panic!("only unit enum column can be indexed")
+                }
                 mapping::Field::Struct(s) => {
                     let embedded_struct = self.app.model(s.id).as_embedded_struct_unwrap();
                     assert!(
