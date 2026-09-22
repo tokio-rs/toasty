afd886978cc71891e1ce3526242784b569fee48f
Carl Lerche <me@carllerche.com>
2026-09-22 21:28:36 +0000

    clippy

--- crates/toasty/src/engine/lower.rs
+++ crates/toasty/src/engine/lower.rs
@@ -992,15 +992,12 @@ impl visit_mut::VisitMut for LowerStatement<'_, '_> {
                             // the enclosing update without reading its returned row.
                             if let Some(update_assignments) =
                                 self.state.update_returning.get(&target_id)
-                            {
-                                if let Some(stmt::Assignment::Set(assigned_value)) =
+                                && let Some(stmt::Assignment::Set(assigned_value)) =
                                     update_assignments.get(&[expr_column.column])
-                                {
-                                    if assigned_value.is_const() {
-                                        *expr = assigned_value.clone();
-                                        return;
-                                    }
-                                }
+                                && assigned_value.is_const()
+                            {
+                                *expr = assigned_value.clone();
+                                return;
                             }
 
                             // the current scope ID should also be the top of the stack
